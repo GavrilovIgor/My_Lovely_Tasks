5f1534379fed41a5b5819e252e7fdcfd0d17f6c6
GeorgiiMarinenko <gmarinenko@mail.ru>
2025-05-01 17:36:18 +0300

    chane script

--- reload.sh
+++ reload.sh
@@ -6,8 +6,11 @@ docker stop my-lovely-tasks-container 2>/dev/null
 # Удалить контейнер (если он существует)
 docker rm my-lovely-tasks-container 2>/dev/null
 
-# Пересобрать образ из текущей директории
-docker build -t my-lovely-tasks-container .
+# Очистить кэш Docker
+docker system prune -f
+
+# Пересобрать образ из текущей директории без использования кэша
+docker build --no-cache -t my-lovely-tasks-container .
 
 # Запустить новый контейнер в фоне
 docker run -d --name my-lovely-tasks-container my-lovely-tasks-container
